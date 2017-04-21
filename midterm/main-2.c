28495bd9ceaa15e3d1a457d03aa7c5dc539a80b3
xris <sir@xris.co>
2017-04-22 06:28:05 +0800

    midterm examinition

---
+++ midterm/main-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char tree[] = "\0ABCDEFG^HIJ^^^^";
+
+void swap(char *lhs, char *rhs) {
+	char t = *lhs; *lhs = *rhs; *rhs = t;
+}
+
+int main() {
+	int len = 16;
+	for (size_t i = 2; i < len; i <<= 1)
+		for (size_t j = 0; j << 1 < i; ++j)
+			swap(&tree[i | j], &tree[(i | j) ^ i - 1]);
+	puts(tree + 1);
+}
---
+++ midterm/main-5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list {
+	struct list *next;
+	long long data;
+};
+
+int main() {
+	struct list *La = malloc(sizeof(struct list));
+	struct list *Lb = malloc(sizeof(struct list));
+	La->data = 0; La->next = NULL;
+	Lb->data = 0; Lb->next = NULL;
+	struct list *Ta = La;
+
+	long long data;
+	while (scanf("%d", &data) != EOF) {
+		Ta->next = malloc(sizeof(struct list));
+		Ta = Ta->next;
+		Ta->data = data;
+		Ta->next = NULL;
+	}
+
+	struct list *p = La, *q = Lb;
+	while (p->next != NULL) {
+		if (~p->next->data & 1) {  // p->data is odd
+			q->next = p->next;
+			p->next = p->next->next;
+			q = q->next;
+			q->next = NULL;
+		} else p = p->next;
+	}
+
+	printf("Odd list: ");
+	for (p = La->next; p != NULL; p = p->next)
+		printf(" %d", p->data);
+	putchar('\n');
+	printf("Even list:");
+	for (q = Lb->next; q != NULL; q = q->next)
+		printf(" %d", q->data);
+	putchar('\n');
+
+	return 0;
+}
